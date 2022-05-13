90187fa93d25f73cff8b0981a78132dbd63dd96f
Rogalowski <rogalowski@gmail.com>
2022-05-13 22:56:21 +0200

    fixed key children list in Expense list by index

--- FRONT/js/components/ExpenseList.js
+++ FRONT/js/components/ExpenseList.js
@@ -3,8 +3,8 @@ import React from "react";
 const ExpenseList = ({ expenses = [] }) => (
     <ol>
         <b>ID. WHEN? --- WHAT? --- HOW MUCH? --- WHO?</b>
-        {expenses.map((expense) => (
-            <li>
+        {expenses.map((expense, index) => (
+            <li key={index}>
                 {expense.date} --- {expense.title} --- {expense.price} zł ---{" "}
                 {expense.userId}
             </li>
