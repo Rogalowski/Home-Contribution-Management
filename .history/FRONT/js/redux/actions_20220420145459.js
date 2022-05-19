6f26b137d20b3c010679a62ce38453b13522db3a
Rogalowski <rogalowski@gmail.com>
2022-05-19 02:55:52 +0200

    README updated

---
+++ .history/FRONT/js/redux/actions_20220420145459.js
@@ -0,0 +1,14 @@
+const ADD_EXPENSE = "ADD_EXPENSE";
+const ADD_USER = "ADD_USER";
+
+const addExpense = (payload) => ({
+    type: ADD_EXPENSE,
+    payload,
+});
+
+const addUser = (payload) => ({
+    type: ADD_USER,
+    payload,
+});
+
+export { ADD_EXPENSE, ADD_USER, addExpense, addUser };
